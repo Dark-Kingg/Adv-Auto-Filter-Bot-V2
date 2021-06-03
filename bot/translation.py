9db7a2c0b2635efd7ac1c0be28a3503fb65b3806
Darkness-kingg <85188845+Darkness-kingg@users.noreply.github.com>
2021-06-03 15:16:15 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,15 +5,18 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+<i>Am Just A  Bot For @Sr_movie_group ....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Just Join On @Sr_Movie_group And See My Pevers 🔥🔥😝
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+My Other Channels And Group 
+https://t.me/utubebot12 https://t.me/SR_MOVIE_GROUP  https://t.me/PEVER_STOCK
+
+Forked By @DARKK_KINGG
 </i>"""    
     
-    HELP_TEXT = """
+    HELP_TEXT = """ I Am NOT Help You Its Just For ME 
+    
 <b><i><u>How To Use Me!?</u></i></b>
 
 <i>
@@ -43,16 +46,19 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@DARKK_KINGG
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/DARKK_KINGG">DARKK_KINGG</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/Dark-Kingg/Adv-Auto-Filter-Bot-V2">Click Me</a></i>
+
+<b>➥ My Channels</b> : <i><a href="https://t.me/utubebot12 https://t.me/SR_MOVIE_GROUP  https://t.me/PEVER_STOCK </a></i>
+
 """
